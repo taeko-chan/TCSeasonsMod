3c2345ebf120ee94aff229167f2c30861770d6cb
taeko_chan <felix.cordone@gmail.com>
2022-09-23 09:20:12 +0200

    added dynamic foliage and grass color

--- src/main/java/ch/taeko/tcseasonsmod/mixin/BiomeMixin.java
+++ src/main/java/ch/taeko/tcseasonsmod/mixin/BiomeMixin.java
@@ -1,12 +1,16 @@
 package ch.taeko.tcseasonsmod.mixin;
 
 import ch.taeko.tcseasonsmod.TCWeatherMod;
+import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.client.color.world.GrassColors;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(Biome.class)
 public class BiomeMixin {
+
     /**
      * @author Taeko
      * @reason Overwrite vanilla, biome dependent/static temperatures.
@@ -21,4 +25,27 @@ public class BiomeMixin {
      */
     @Overwrite
     public float getDownfall() { return (float)TCWeatherMod.currentDownfall; }
+
+    /**
+     * @author Taeko
+     * @reason Use new temperature and downfall
+     */
+    @Overwrite
+    private int getDefaultGrassColor() {
+        double d = MathHelper.clamp(TCWeatherMod.currentTemperature, 0.0F, 1.0F);
+        double e = MathHelper.clamp(TCWeatherMod.currentDownfall, 0.0F, 1.0F);
+        return GrassColors.getColor(d, e);
+    }
+
+    /**
+     * @author Taeko
+     * @reason Use new temperature and downfall
+     */
+    @Overwrite
+    private int getDefaultFoliageColor() {
+        double d = MathHelper.clamp(TCWeatherMod.currentTemperature, 0.0F, 1.0F);
+        double e = MathHelper.clamp(TCWeatherMod.currentDownfall, 0.0F, 1.0F);
+        return FoliageColors.getColor(d, e);
+    }
+
 }
